46f88af71f83f486dcb131bf0d67e5d4198ed237
FuturisticGoo <fgoo.edu@hash.fyi>
2021-02-20 08:02:53 -0800

    Added comments

--- restricted_input/__init__.py
+++ restricted_input/__init__.py
@@ -5,6 +5,9 @@ Created on Sun Jan 31 21:32:05 2021
 @author: FuturisticGoo
 
 Contains two functions r_input() and get_input_types().
+r_input() is used for restricting input. Do help(r_input) for
+more information.
+get_input_types() is getting a list of input types.
 """
 
 import platform
@@ -53,7 +56,7 @@ allowed. The default is "".
         import msvcrt
         is_windows = True
     else:
-        # Little bit different for Linux or macOS
+        # Little bit different getch for Linux or macOS
         def posix_getch():
             import termios
             import tty
@@ -69,28 +72,33 @@ allowed. The default is "".
 
     print(prompt, end="", flush=True)
     ask = ""
+
     while True:
 
         if(is_windows):
             temp = msvcrt.getch().decode("UTF-8")
         else:
             temp = posix_getch()
-
+        # Now handling certain special inputs
         if(temp == "\r"):
             break
-        elif(temp == "\x03"):
+        elif(temp == "\x03"): # CTRL+C
             raise KeyboardInterrupt
-        elif(temp in ["\b", "\x7f"]):
+        elif(temp in ["\b", "\x7f"]): # Backspace
             if(len(ask) != 0):
                 print("\b \b", end="", flush=True)
                 ask = ask[:-1]
         else:
             if(maxlength == -1 or len(ask) < maxlength):
-                if(input_type == "normal"):
+                #Now restricting input 
+                
+                if(input_type == "normal"): 
+                    # Allows any input
                     ask += temp
                     print(temp, end="", flush=True)
 
-                elif(input_type == "string_all"):
+                elif(input_type == "string_all"): 
+                    # Allows strings. Exceptions can be added
                     if(temp.isalpha() or temp in exlude):
                         ask += temp
                         print(temp, end="", flush=True)
@@ -102,6 +110,7 @@ allowed. The default is "".
                         pass
 
                 elif(input_type == "string_upper"):
+                    # Allows only uppercase strings. Exceptions can be added
                     if((temp.isalpha() and temp.isupper()) or temp in exlude):
                         ask += temp
                         print(temp, end="", flush=True)
@@ -113,6 +122,7 @@ allowed. The default is "".
                         pass
 
                 elif(input_type == "string_lower"):
+                    # Allows only lowercase strings. Exceptions can be added
                     if((temp.isalpha() and temp.islower()) or temp in exlude):
                         ask += temp
                         print(temp, end="", flush=True)
@@ -124,6 +134,8 @@ allowed. The default is "".
                         pass
 
                 elif(input_type == "specials"):
+                    # Allows only special characters defined in list special_char.
+                    # Exceptions can be added
                     if(temp in special_char or temp in exlude):
                         ask += temp
                         print(temp, end="", flush=True)
@@ -135,6 +147,8 @@ allowed. The default is "".
                         pass
 
                 elif(input_type == "integer"):
+                    # Allows only integers. Full stop/period and comma not allowed
+                    # Exceptions can be added
                     if(temp.isdigit() or temp in exlude):
                         ask += temp
                         print(temp, end="", flush=True)
@@ -146,6 +160,8 @@ allowed. The default is "".
                         pass
 
                 elif(input_type == "float"):
+                    # Allows integers and one instance of full stop/period
+                    # Comma not allowed. Exceptions can be added
                     if((temp.isdigit() or temp == ".") or temp in exlude):
                         if(ask.find(".") == -1):
                             ask += temp
@@ -159,7 +175,10 @@ allowed. The default is "".
                         print(warning+ask, end="", flush=True)
                     else:
                         pass
+
                 elif(input_type == "version"):
+                    # Allows integers and any number of full stop/period
+                    # Comma not allowed. Exceptions can be added
                     if((temp.isdigit() or temp == ".") or temp in exlude):
                         ask += temp
                         print(temp, end="", flush=True)
@@ -169,7 +188,12 @@ allowed. The default is "".
                         print(warning+ask, end="", flush=True)
                     else:
                         pass
+
                 elif(input_type == "nothing"):
+                    # Nothing is allowed to be entered as input.
+                    # Useful in scenarios where you only need input
+                    # like 1/2/3, by adding exclude="123" to the 
+                    # r_input arguments
                     if(temp in exlude):
                         ask += temp
                         print(temp, end="", flush=True)
@@ -198,6 +222,7 @@ def get_input_types():
 
 
 if __name__ == "__main__":
+    # Cycling through all the input types
     for i in get_input_types():
         print(r_input(f"Enter {i}: ", i,
                       warning=f"Only {i} allowed. Enter {i}: "))
